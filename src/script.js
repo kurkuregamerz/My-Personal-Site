dbfed42a4e9e312a4b612d3e049c1028a5a97b3b
kurkuregamerz <crazyatozy@gmail.com>
2026-07-04 10:52:56 +0530

    5

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const main = document.getElementById("main")
+const hero_section_main = document.getElementById("hero_section_main");
+const owners_image_hero_section = document.getElementById("owners_image_hero_section");
+const owners_name_hero_section = document.getElementById("owners_name_hero_section")
+
+// hero_section_main.addEventListener("focus",()=>{
+//     owners_image_hero_section.classList.add("hero_section_owners_image_transition")
+//     owners_name_hero_section.classList.add("hero_section_owners_name_transition")
+// })
