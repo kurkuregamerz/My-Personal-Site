cc645c68c643a5f3fd8e74f964de74b26772d795
kurkuregamerz <crazyatozy@gmail.com>
2026-07-06 22:06:35 +0530

    initial commit

---
+++ src/components/loader.js
@@ -0,0 +1,21 @@
+export function loader() {
+    return  `<div id="loader">
+  <div class="cell d-0"></div>
+  <div class="cell d-1"></div>
+  <div class="cell d-2"></div>
+
+  <div class="cell d-1"></div>
+  <div class="cell d-2"></div>
+  
+  
+  <div class="cell d-2"></div>
+  <div class="cell d-3"></div>
+  
+  
+  <div class="cell d-3"></div>
+  <div class="cell d-4"></div>
+  
+  
+</div>
+   `
+}
--- src/scripts/home_script.js
+++ src/scripts/home_script.js
@@ -1,6 +1,11 @@
-import { navbar_creator } from '../src/components/navbar_exporter.js';
+import { loader } from '../components/loader.js'
+import { navbar_creator } from '../components/navbar_exporter.js';
 
+document.body.innerHTML += loader();
+
+const loader_main = document.getElementById("loader");
 const main = document.getElementById("main")
+
 const hero_section_main = document.getElementById("hero_section_main");
 const owners_image_hero_section = document.getElementById("owners_image_hero_section");
 const owners_transparentname_hero_section = document.getElementById("owners_transparentname_hero_section")
@@ -12,6 +17,13 @@ const navbar_navigation_menu_button = document.getElementById("navbar_navigation
 const navbar_menu_navigation_button_line1 = document.getElementById("navbar_menu_button_line1")
 const navbar_menu_navigation_button_line2 = document.getElementById("navbar_menu_button_line2");
 
+
+main.style.display = "none";
+window.addEventListener("load", () =>{
+    loader_main.style.display = "none";
+    main.style.display = "block";
+    
+})
 
 let navbar_navigation_menu_button_click_count = 0;
 navbar_navigation_menu_button.addEventListener("click", () => {
