0f341cb004d82808655d097f8970a4b51262047f
kurkuregamerz <crazyatozy@gmail.com>
2026-07-09 09:08:26 +0530

    initial commit

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,8 +1,17 @@
+document.head.innerHTML += `<style id="hold-animation-loader">
+#main *{
+animation-play-state: paused !important;
+transition:none !important;
+}
+</style>`
 document.body.innerHTML += `<div id="loader" ></div>`;
 const loader_main = document.getElementById("loader");
 const main = document.getElementById("main");
 window.addEventListener("load", () => {
-  
+  if(
+    document.getElementById("hold-animation-loader")) {
+    document.getElementById("hold-animation-loader").remove()
+  }
   loader.style.animationName = "loader_exit"
 
   setTimeout(() => {
