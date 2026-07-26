9800abdb5026bcfd3868099f19dc4a13d09211ac
kurkuregamerz <crazyatozy@gmail.com>
2026-07-26 10:51:05 +0530

    navbar bug fixed

--- src/components/navbar/navbar_exporter.js
+++ src/components/navbar/navbar_exporter.js
@@ -52,7 +52,7 @@ export function navbar_creator() {
             Contact Phone
             <a href="tel:+917003555728" id="call_nav_dropdown_section2">+91 70035 55728</a>
             </div>
-            <div id="contact_phone_nav_dropdown">
+            <div id="contact_email_nav_dropdown">
             Contact Email
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=crazyatozy@gmail.com" id="email_nav_dropdown_section2">crazyatozy@gmail.com</a>
             </div>
@@ -69,72 +69,64 @@ export function navbar_creator() {
 }
 export const css_file_link_navbar = `<link rel="stylesheet" href="../components/navbar/navbar_style.css">`;
 
-
-
-
 export function Event_listener_config_creator_dropdown_nav() {
-    
- const dropdown_menu_navbar = document.getElementById("dropdown_menu_navbar");
- 
+  const dropdown_menu_navbar = document.getElementById("dropdown_menu_navbar");
 
- const navbar_navigation_menu_button = document.getElementById(
-  "navbar_navigation_menu_button",
-);
-let navbar_navigation_menu_button_click_count = 0;
-    navbar_navigation_menu_button.addEventListener("click", () => {
-        const navbar_menu_navigation_button_line1 = document.getElementById(
-            "navbar_menu_button_line1",
+  const navbar_navigation_menu_button = document.getElementById(
+    "navbar_navigation_menu_button",
+  );
+  let navbar_navigation_menu_button_click_count = 0;
+  const navbar_navigation_menu_button_event_listener_callback = () => {
+    const navbar_menu_navigation_button_line1 = document.getElementById(
+      "navbar_menu_button_line1",
+    );
+    const navbar_menu_navigation_button_line2 = document.getElementById(
+      "navbar_menu_button_line2",
+    );
+
+    function body_event_listener_dropdownmenu_nav(event) {
+      if (Event.target == dropdown_menu_navbar) {
+        return null;
+      } else {
+        navbar_menu_navigation_button_line1.style.transform = `rotate(0deg)`;
+        navbar_menu_navigation_button_line2.style.transform = `rotate(0deg)`;
+        dropdown_menu_navbar.style.height = "0%";
+        dropdown_menu_navbar.style.display = "none";
+        document.body.removeEventListener(
+          "click",
+          body_event_listener_dropdownmenu_nav,
         );
-        const navbar_menu_navigation_button_line2 = document.getElementById(
-            "navbar_menu_button_line2",
+      }
+    }
+    if (navbar_navigation_menu_button_click_count === 0) {
+      navbar_navigation_menu_button_click_count = 1;
+      setTimeout(() => {
+        document.body.addEventListener(
+          "click",
+          body_event_listener_dropdownmenu_nav,
         );
-
-        function body_event_listener_dropdownmenu_nav() {
-            navbar_menu_navigation_button_line1.style.transform = `rotate(0deg)`;
-            navbar_menu_navigation_button_line2.style.transform = `rotate(0deg)`;
-            dropdown_menu_navbar.style.height = "0%";
-            dropdown_menu_navbar.style.visibility = "hidden";
-            document.body.removeEventListener(
-                "click",
-                body_event_listener_dropdownmenu_nav,
-            );
-        }
-        if (navbar_navigation_menu_button_click_count == 0) {
-            setTimeout(() => {
-                document.body.addEventListener(
-                    "click",
-                    body_event_listener_dropdownmenu_nav,
-                );
-            }, 500);
-            navbar_menu_navigation_button_line1.style.transform = `rotate(-120deg)`;
-          navbar_menu_navigation_button_line2.style.transform = `rotate(120deg)`;
-          if (screen.orientation.type == "portrait-primary") {
-            dropdown_menu_navbar.style.height = "100%";
-            
-            
-          }
-          else if(screen.orientation.type == "landscape-primary") {
-            dropdown_menu_navbar.style.height = "90%";
-            
-          }
-          dropdown_menu_navbar.style.display = "grid";
-          dropdown_menu_navbar.style.visibility = "visible";
-          dropdown_menu_navbar.style.transform = "skewY(100%)"
-            navbar_navigation_menu_button_click_count = 1;
-        } else {
-            document.body.removeEventListener(
-                "click",
-                body_event_listener_dropdownmenu_nav,
-            );
-            navbar_menu_navigation_button_line1.style.transform = `rotate(0deg)`;
-            navbar_menu_navigation_button_line2.style.transform = `rotate(0deg)`;
-          dropdown_menu_navbar.style.height = "0%";
-          dropdown_menu_navbar.style.opacity = "100%"
-          dropdown_menu_navbar.style.visibility = "hidden";
-          dropdown_menu_navbar.style.display = "none";
-          
-            navbar_navigation_menu_button_click_count = 0;
-        }
-    });
-};
-
+      }, 100);
+      navbar_menu_navigation_button_line1.style.transform = `rotate(-120deg)`;
+      navbar_menu_navigation_button_line2.style.transform = `rotate(120deg)`;
+      if (screen.orientation.type == "portrait-primary") {
+        dropdown_menu_navbar.style.height = "100%";
+      } else if (screen.orientation.type == "landscape-primary") {
+        dropdown_menu_navbar.style.height = "90%";
+      }
+      dropdown_menu_navbar.style.display = "grid";
+      
+    } else {
+      navbar_navigation_menu_button_click_count = 0;
+      document.body.removeEventListener(
+        "click",
+        body_event_listener_dropdownmenu_nav,
+      );
+      navbar_menu_navigation_button_line1.style.transform = `rotate(0deg)`;
+      navbar_menu_navigation_button_line2.style.transform = `rotate(0deg)`;
+      dropdown_menu_navbar.style.height = "0%";
+      dropdown_menu_navbar.style.display = "none";
+      
+    }
+  }
+  navbar_navigation_menu_button.addEventListener("click", navbar_navigation_menu_button_event_listener_callback);
+}
--- src/scripts/global_script.js
+++ src/scripts/global_script.js
@@ -1,11 +1,16 @@
 import { navbar_creator, css_file_link_navbar, Event_listener_config_creator_dropdown_nav } from "../components/navbar/navbar_exporter.js";
 import {
     footer_creator, css_file_link_footer
-} from "../components/footer/footer_exporter.js"
+} from "../components/footer/footer_exporter.js";
+
 const main = document.getElementById("main");
+const head = document.head;
 main.innerHTML += navbar_creator();
-document.head.innerHTML += css_file_link_navbar;
-Event_listener_config_creator_dropdown_nav();
-// ============================================><
+head.innerHTML += css_file_link_navbar;
+setTimeout(() => {
+    Event_listener_config_creator_dropdown_nav()
+}, 100);
+
+// ============================================>
 main.innerHTML += footer_creator();
-document.head.innerHTML += css_file_link_footer;
+head.innerHTML += css_file_link_footer;
